c91c237c505c5429e96d33abf649854a1b867609
Rudransh <rudraman.2805+dev@gmail.com>
2024-12-18 18:34:08 +0530

    Add DB_PORT to environment variables and update database connection settings

--- backend/dbUtils.js
+++ backend/dbUtils.js
@@ -9,7 +9,8 @@ async function connectDb() {
             host: 'localhost',
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
-            database: process.env.DB_NAME
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT
         });
         log('Connected to MySQL database');
         const tables = [
